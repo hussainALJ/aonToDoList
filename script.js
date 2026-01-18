4def601d06fbbda7db7cdd52431039cb4df3d55b
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-18 18:24:15 +0300

    Add Header hero and style

--- script.js
+++ script.js
@@ -26,8 +26,15 @@ function printNote() {
         const note = document.createElement("li");
         note.className = "note";
         note.id = index;
-        note.textContent = val;
-        note.innerHTML += `<button class = 'delete' onclick = 'deleteNote(${note.id})'>X</button>`;
+        note.innerHTML += `<button class = 'delete' onclick = 'deleteNote(${note.id})'><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#FF0000"/>
+        <line x1="9.70711" y1="9.00002" x2="23" y2="22.2929" stroke="#FF0000" stroke-linecap="round"/>
+        <line x1="9" y1="22.2929" x2="22.2929" y2="9" stroke="#FF0000" stroke-linecap="round"/>
+        </svg>
+        </button>`;
+        const p = document.createElement("p");
+        p.textContent = val;
+        note.appendChild(p);
         toDo.appendChild(note);
     })
     upDateCounter();
