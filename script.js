34965cf4429df4e24d77ee435bccce95202a42df
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-18 11:55:32 +0300

    Redo the todo section

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-const notes = document.querySelector(`#notes`);
+const toDoArr = localStorage.getItem("notes") === null? []: JSON.parse(localStorage.getItem("notes"))
+
+const toDo = document.querySelector(`#toDoList`);
 const text = document.querySelector(`#text`);
 const addBtn = document.querySelector(`#add`);
+const counter = document.querySelector('#total')
+
+text.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        addNote();
+    }
+})
 
 function addNote() {
-    storeNote(localStorage.length);
-    showNotes()
-    
+    toDoArr.unshift(text.value);
+    text.value = '';
+    save()
+    printNote();
+    upDateCounter()
+    console.log(toDoArr);
 }
 
-function storeNote(id) {
-    localStorage.setItem(`${id}`, text.value);
-    noteCounter();
+function printNote() {
+    toDo.textContent = '';
+    toDoArr.forEach((val, index) => {
+        const note = document.createElement("li");
+        note.className = "note";
+        note.id = index;
+        note.textContent = val;
+        note.innerHTML += `<button class = 'delete' onclick = 'deleteNote(${note.id})'>X</button>`;
+        toDo.appendChild(note);
+    })
+    upDateCounter();
 }
 
-function showNotes() {
-    notes.innerHTML = "";
-    for (let i = localStorage.length - 1; i >= 0; i--) {
-        const newNote = document.createElement("li");
-        newNote.innerHTML = `${localStorage.getItem(i)} <button class = "delete" onclick = "deleteNote(${i})"> X </button>`;
-        notes.appendChild(newNote);
-    }
+function save() {
+    localStorage.setItem("notes", JSON.stringify(toDoArr))
 }
 
-function deleteNote(noteId) {
-    localStorage.removeItem(noteId);
-    showNotes();
-    noteCounter();
+function deleteNote(note) {
+    toDoArr.splice(note, 1);
+    printNote()
+    save();
 }
 
-function noteCounter() {
-    const counter = document.querySelector("#total");
-    counter.innerHTML = `Total list: <span>${localStorage.length}</span>`
+function upDateCounter() {
+    counter.innerHTML = `Total list: <span>${toDoArr.length}</span>`
 }
 
-showNotes();
-noteCounter();
-
+printNote();
+upDateCounter();
